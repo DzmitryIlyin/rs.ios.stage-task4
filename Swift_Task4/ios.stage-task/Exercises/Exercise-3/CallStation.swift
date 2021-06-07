3cf2623ac8f9025db0e681a2da76876cc70913d2
Dzmitry Ilyin <Dzmitry_Ilyin@epam.com>
2021-06-07 11:28:08 +0300

    rearrange funcs

--- Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
@@ -27,6 +27,48 @@ extension CallStation: Station {
         }
     }
     
+    private func areAllUsersAdded(incomingUser: User, outgoingUser: User) -> Bool {
+        return usersArray.contains(where: {$0.id == incomingUser.id}) && usersArray.contains(where: {$0.id == outgoingUser.id})
+    }
+
+    private func cleanPreviousCallState(userType: UserType, user: User) {
+        if userType == .incoming {
+            callsArray.removeAll{ $0.incomingUser.id == user.id }
+        } else {
+            callsArray.removeAll{ $0.outgoingUser.id == user.id }
+        }
+    }
+    
+    func calls() -> [Call] {
+        return callsArray
+    }
+    
+    private func userType(_ currenCall: Call, _ user: User) -> (UserType, User, User) {
+        if currenCall.incomingUser.id == user.id {
+            return (UserType.incoming, user, currenCall.outgoingUser)
+        } else {
+            return (UserType.outgoing, user, currenCall.incomingUser)
+        }
+    }
+    
+    func call(id: CallID) -> Call? {
+        return callsArray.first{$0.id == id}
+    }
+    
+    func currentCall(user: User) -> Call? {
+        if let call = callsArray.first(where: { ($0.incomingUser.id == user.id || $0.outgoingUser.id == user.id) && ($0.status == .calling || $0.status == .talk)}) {
+            return call
+        } else {
+            return nil
+        }
+    }
+    
+    private enum UserType {
+        case incoming
+        case outgoing
+        case none
+    }
+    
     func execute(action: CallAction) -> CallID? {
         
         var callID: CallID?
@@ -77,45 +119,4 @@ extension CallStation: Station {
         return status != .ended(reason: .error) ? callID : nil
     }
     
-    private func areAllUsersAdded(incomingUser: User, outgoingUser: User) -> Bool {
-        return usersArray.contains(where: {$0.id == incomingUser.id}) && usersArray.contains(where: {$0.id == outgoingUser.id})
-    }
-
-    private func cleanPreviousCallState(userType: UserType, user: User) {
-        if userType == .incoming {
-            callsArray.removeAll{ $0.incomingUser.id == user.id }
-        } else {
-            callsArray.removeAll{ $0.outgoingUser.id == user.id }
-        }
-    }
-    
-    func calls() -> [Call] {
-        return callsArray
-    }
-    
-    private func userType(_ currenCall: Call, _ user: User) -> (UserType, User, User) {
-        if currenCall.incomingUser.id == user.id {
-            return (UserType.incoming, user, currenCall.outgoingUser)
-        } else {
-            return (UserType.outgoing, user, currenCall.incomingUser)
-        }
-    }
-    
-    func call(id: CallID) -> Call? {
-        return callsArray.first{$0.id == id}
-    }
-    
-    func currentCall(user: User) -> Call? {
-        if let call = callsArray.first(where: { ($0.incomingUser.id == user.id || $0.outgoingUser.id == user.id) && ($0.status == .calling || $0.status == .talk)}) {
-            return call
-        } else {
-            return nil
-        }
-    }
-    
-    private enum UserType {
-        case incoming
-        case outgoing
-        case none
-    }
 }
